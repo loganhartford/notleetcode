548f1ee9ab9f8474ad8bc247b80bb66ae8d5c30e
loganhartford <logan.hartford@outlook.com>
2026-07-10 09:14:28 -0700

    first hard one

--- solutions/fw-sizeof-trick.c
+++ solutions/fw-sizeof-trick.c
@@ -2,15 +2,21 @@
 #include <stddef.h>
 
 /*
- * Define MY_SIZEOF(T) so it returns the byte size of type T
+ * Define MY_SIZEOF_TYPE(T) so it returns the byte size of type T
  * without using the sizeof operator.
  */
-#define MY_SIZEOF(T) ((size_t)((char *)((T*)0+1)-(char *)(T *)0))
 
-#define PTR_TO_T_ZERO(T)        ((T *)0)
-#define PTR_TO_T_ONE_PAST(T)    (PTR_TO_T_ZERO(T) + 1)
 
-#define BYTE_PTR_ZERO(T)        ((char *)PTR_TO_T_ZERO(T))
-#define BYTE_PTR_ONE_PAST(T)    ((char *)PTR_TO_T_ONE_PAST(T))
+/*
+ * So basically we cast a pretend address 0 to "T", which is a type in this case.
+ * Let's say that it's and int. then we have (int *)0, so we have an int pointer
+ * at address 0.
+ * 
+ * Then we do the same thing and increment by 1. Pointer arithmetic moves the pointer
+ * by the size of the type. so for and int, (int *)0 + 1 = 4 on a 32 bit system.
+ * 
+ * Then we cast to a char * because char pointers are a single byte so the arithmetic will
+ * be byte based. When we subract them, we'll bet 4 - 0 = 4 which is the byte length of the int.
+ */
 
-#define MY_SIZEOF(T)            ((size_t)(BYTE_PTR_ONE_PAST(T) - BYTE_PTR_ZERO(T)))
+#define MY_SIZEOF_TYPE(T) ((size_t)(((char *)(((T *)0) + 1)) - ((char *)((T *) 0))))
---
+++ solutions/fw-sizeof-var.c
@@ -0,0 +1,4 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#define MY_SIZEOF_VAR(x) 
