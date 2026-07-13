b0244b45fd118304165f71b7aae9a0944917a493
loganhartford <logan.hartford@outlook.com>
2026-07-13 10:43:40 -0700

    progress:

--- solutions/logan/fw-pool-alloc.c
+++ solutions/logan/fw-pool-alloc.c
@@ -13,24 +13,49 @@ bool pool_init(pool_t *pool, void *storage, size_t storage_size, size_t block_si
     if (storage == NULL) return false;
     if (storage_size < block_size) return false;
     if (block_size < sizeof(void *)) return false;
+
     pool->free_list = storage;
     pool->block_size = block_size;
-    pool->total = storage_size;
-    pool->free_count = pool->total / pool->block_size;
+    pool->free_count = storage_size / pool->block_size;
+    pool->total = pool->free_count;
     
-    printf("%d", pool->total / pool->block_size);
+    uint8_t *base = storage;
+    for (int i = 0; i < pool->free_count; i++) {
+        uint8_t *current = base + i * block_size;
+        *(void **)current = current + block_size;
+    }
+    
+    /*
+        Plan:
+        - Basically store the next address in each block
+        - The address of p->free_list points to the current free section,
+          the value of *p->free_list points to the next free section.
+        - When allocating, just need to store *p->free_list, then zero it, the set p->free_list to the stored address
+        - When freeing, write the address of the current free block, as the value block being freed and see free pointer
+          to current block.
+    */
     
     return true;
 }
 
 void *pool_alloc(pool_t *pool) {
-    // TODO
-    return NULL;
+    if (!pool->free_count) return NULL;
+
+    void *new_free = (void **)pool->free_list;
+    *(void **)pool->free_list = 0;
+    void * allocated = pool->free_list;
+    pool->free_list = new_free;
+    pool->free_count--;
+    return allocated;
 }
 
 bool pool_free(pool_t *pool, void *ptr) {
-    if (pool->free_count) return true;
-    return false;
+    if (ptr == NULL) return false;
+    void * old_free = pool->free_list;
+    *(void **)ptr = old_free;
+    pool->free_list = ptr;
+    pool->free_count++;
+    return true;
 }
 
 size_t pool_free_count(const pool_t *pool) {
